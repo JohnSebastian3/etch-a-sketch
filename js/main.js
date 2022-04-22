230136c49e39729556172334f09b0d8abf6d9907
Sebastian <sebastianbiochem@gmail.com>
2022-04-21 20:28:32 -0400

    Add ability to change grid size

--- js/main.js
+++ js/main.js
@@ -3,25 +3,62 @@ const clearButton = document.querySelector('.clear');
 
 clearButton.addEventListener('click', eraseBoard);
 
-for(let i = 0; i < 256; i++) {
-  let square = document.createElement('div');
-  square.classList.add('square');
-  square.style.border = '1px solid black';
-  gridContainer.appendChild(square);
+function createGrid(sideLength) {
+  removeAllChildNodes(gridContainer);
+
+  gridContainer.style['grid-template-columns'] = `repeat(${sideLength}, 1fr)`;
+  gridContainer.style['grid-template-rows'] = `repeat(${sideLength}, 1fr)`;
+
+  const totalSquares = sideLength ** 2;
+  for(let i = 0; i < totalSquares; i++) {
+    let square = document.createElement('div');
+    square.classList.add('square');
+    square.style.border = '1px solid black';
+    gridContainer.appendChild(square);
+  }
+
+  const squares = document.querySelectorAll('.square');
+  console.log(squares);
+
+  for(let square of squares) {
+    square.addEventListener('mouseenter', filled);
+  }
+
 }
 
-const squares = document.querySelectorAll('.square');
-
-for(let square of squares) {
-  square.addEventListener('mouseenter', filled);
+function removeAllChildNodes(parent) {
+  while(parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
 }
+
+createGrid(16)
+
+// const squares = document.querySelectorAll('.square');
+
+// for(let square of squares) {
+//   square.addEventListener('mouseenter', filled);
+// }
 
 function filled(e) {
   e.target.classList.add('filled');
 }
 
+
+
 function eraseBoard(e) {
+  console.log(e);
+  const squares = document.querySelectorAll('.square');
   for(let square of squares) {
     square.classList.remove('filled');
   }
+
+  let sideLength;
+
+  do {
+    sideLength = +prompt('Enter number of squares per side to generate new grid: ');
+  } while(sideLength > 100 || sideLength < 0);
+
+  createGrid(sideLength);
+
 }
